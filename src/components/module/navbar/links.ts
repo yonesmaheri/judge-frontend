e09e92b2f721b3f0def97267e729513049433cd6
Yones Maheri <yonesmaheri80@gmail.com>
2025-11-16 10:47:31 +0330

    remove code judgung system

--- src/components/module/navbar/links.ts
+++ src/components/module/navbar/links.ts
@@ -1,6 +1,5 @@
 export const links = [
     { href: "/", label: "Home" },
-    { href: "/questions", label: "Questions (Code juding system)" },
     { href: "/resume", label: "Resume" },
     { href: "/projects", label: "Projects" },
   ];
